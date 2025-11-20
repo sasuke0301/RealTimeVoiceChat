1598679b0e2fd1b9033769205df17f84f827bd5b
sasuke <t0p.sasuke.kss@gmail.com>
2025-11-20 10:36:59 +0900

    fix

--- src/pages/ConsolePage.tsx
+++ src/pages/ConsolePage.tsx
@@ -663,6 +663,12 @@ export function ConsolePage() {
             </div>
           </div>
           <div className="content-actions">
+            <Toggle
+              defaultValue={false}
+              labels={['manual', 'vad']}
+              values={['none', 'server_vad']}
+              onChange={(_, value) => changeTurnEndType(value)}
+            />
             <div className="spacer" />
             {isConnected && canPushToTalk && (
               <Button
@@ -683,6 +689,40 @@ export function ConsolePage() {
                 isConnected ? disconnectConversation : connectConversation
               }
             />
+          </div>
+        </div>
+        <div className="content-right">
+          <div className="content-block map">
+            <div className="content-block-title">get_weather()</div>
+            <div className="content-block-title bottom">
+              {marker?.location || 'not yet retrieved'}
+              {!!marker?.temperature && (
+                <>
+                  <br />
+                  🌡️ {marker.temperature.value} {marker.temperature.units}
+                </>
+              )}
+              {!!marker?.wind_speed && (
+                <>
+                  {' '}
+                  🍃 {marker.wind_speed.value} {marker.wind_speed.units}
+                </>
+              )}
+            </div>
+            <div className="content-block-body full">
+              {coords && (
+                <Map
+                  center={[coords.lat, coords.lng]}
+                  location={coords.location}
+                />
+              )}
+            </div>
+          </div>
+          <div className="content-block kv">
+            <div className="content-block-title">set_memory()</div>
+            <div className="content-block-body content-kv">
+              {JSON.stringify(memoryKv, null, 2)}
+            </div>
           </div>
         </div>
       </div>
